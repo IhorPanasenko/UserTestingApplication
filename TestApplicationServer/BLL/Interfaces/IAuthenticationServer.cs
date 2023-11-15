25b04f1d4a4ff9a51c4fd9936c30e84487204722
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-15 23:37:22 +0200

    Create interface for authentication server

---
+++ TestApplicationServer/BLL/Interfaces/IAuthenticationServer.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Core.Models;
+
+namespace BLL.Interfaces
+{
+    public interface IAuthenticationServer
+    {
+        public Task<bool> RegisterAsync(RegisterUserModel registerUser);
+
+        public Task<string?> LoginAsync(LoginUserModel loginUser);
+    }
+}
