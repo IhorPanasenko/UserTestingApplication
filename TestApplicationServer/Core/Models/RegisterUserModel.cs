2fb57aa9a5940c54f87efc67b4f388f6e1efe8f6
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-15 23:30:38 +0200

    Create RegisterUserModel.cs

---
+++ TestApplicationServer/Core/Models/RegisterUserModel.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Core.Models
+{
+    public class RegisterUserModel
+    {
+        public string? Login { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? ConfirmPassword { get; set; }
+
+        public string? Role { get; set; }
+    }
+}
