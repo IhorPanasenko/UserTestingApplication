97f8b6f763fce5f2f6947c038a055c48d23b686b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 12:56:30 +0200

    Add datasets for models

--- TestApplicationServer/DAL/ApplicationDbContext.cs
+++ TestApplicationServer/DAL/ApplicationDbContext.cs
@@ -7,10 +7,16 @@ namespace DAL
     public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<UserTest> UserTests { get; set; }
+        public DbSet<QuestionType> QuestionTypes { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionOption> QuestionIption {  get; set; }
+        public DbSet<UserAnswer> UserAnswers { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-                
+            
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
