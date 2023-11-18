7b8364639d34029124fb3135d491e3453f481b69
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 19:24:51 +0200

    Implement IQuestionRepository

---
+++ TestApplicationServer/DAL/Repositories/QuestionRepository.cs
@@ -0,0 +1,37 @@
+﻿using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class QuestionRepository : IQuestionRepository
+    {
+        private readonly ILogger<QuestionRepository> logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public QuestionRepository(ILogger<QuestionRepository> logger, ApplicationDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<int> CountTestQuestions(int testId)
+        {
+            try
+            {
+                var numberOfQuestoins = await dbContext.Questions.Where(q => q.TestId == testId).CountAsync();
+                return numberOfQuestoins;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError($"Error while getting data from db: {ex.Message}");
+                return 0;
+            }
+        }
+    }
+}
