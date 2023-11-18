9391ed09baf8b647f9b9ba778d3f0b6d432b629a
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 19:37:01 +0200

    Implement ITestRepository

---
+++ TestApplicationServer/DAL/Repositories/TestRepository.cs
@@ -0,0 +1,37 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class TestRepository : ITestRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly ILogger<TestRepository> logger;
+
+        public TestRepository(ILogger<TestRepository> logger, ApplicationDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Test?> GetById(int testId)
+        {
+            try
+            {
+                var test = await dbContext.Tests.FindAsync(testId);
+                return test;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError($"Error while getting data from db: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
