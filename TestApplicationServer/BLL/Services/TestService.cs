eae06ce2a4411244aab9e55ffbf0d1ee37ee0172
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 20:51:25 +0200

    Implement ITestService

---
+++ TestApplicationServer/BLL/Services/TestService.cs
@@ -0,0 +1,38 @@
+﻿using BLL.Interfaces;
+using Core.Models;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class TestService:ITestService
+    {
+        private readonly ILogger<TestService> logger;
+        private readonly ITestRepository testRepository;
+
+        public TestService(ILogger<TestService> logger, ITestRepository testRepository)
+        {
+            this.logger = logger;
+            this.testRepository = testRepository;
+        }
+
+        public async Task<Test?> GetById(int testId)
+        {
+            try
+            {
+                var test = await testRepository.GetById(testId);
+                return test;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError($"Error in data access layer: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
