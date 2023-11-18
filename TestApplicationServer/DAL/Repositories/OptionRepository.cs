0eac7c892a25b0683ebbcb1215df44b7e215342d
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 00:44:23 +0200

    Implement IOptionRepository

---
+++ TestApplicationServer/DAL/Repositories/OptionRepository.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DAL.Repositories
+{
+    internal class OptionRepository : IOptionRepository
+    {
+        private readonly ILogger<OptionRepository> logger;
+        private readonly ApplicationDbContext dbContext;
+        public OptionRepository(ILogger<OptionRepository> logger, ApplicationDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<QuestionOption>?> GetByQuestion(int questionId)
+        {
+            try
+            {
+                var options = await dbContext.QuestionOption.Where(qo => qo.QuestionId == questionId).ToListAsync();
+                return options;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError($"Error while getting data from db {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
