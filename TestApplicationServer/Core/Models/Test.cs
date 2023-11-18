ec2ace832ea4ca7e1dafc4c9991f6c3f845dae66
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 11:39:49 +0200

    Create Model For Test

---
+++ TestApplicationServer/Core/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class Test
+    {
+        [Key]
+        public int TestId { get; set; }
+
+        [Required]
+        public string TestName { get; set; } = string.Empty;
+    }
+}
