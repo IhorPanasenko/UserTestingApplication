904cd87d60e207b4db2b65b809e59f9101c24972
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 16:53:00 +0200

    Create UserTestService.cs

---
+++ TestApplicationServer/BLL/Services/UserTestService.cs
@@ -0,0 +1,29 @@
+﻿using BLL.Interfaces;
+using Core.Models;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class UserTestService: IUserTestService
+    {
+        public readonly IUserTestRepository userTestRepository;
+        public readonly ILogger<UserTestService> logger;
+
+        public UserTestService(ILogger<UserTestService> logger, IUserTestRepository userTestRepository)
+        {
+            this.logger = logger;
+            this.userTestRepository = userTestRepository;
+        }
+
+        public Task<List<UserTest>> GetUserTests(string userId)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
