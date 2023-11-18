d4e15ea0ef4328c8900d6ca5f53116afaa428dd2
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 11:40:58 +0200

    Create UserTest Model

---
+++ TestApplicationServer/Core/Models/UserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class UserTest
+    {
+        [Key]
+        public int UserTestId { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        [ForeignKey("Test")]
+        public int TestId { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public int? Mark { get; set; }
+    }
+}
