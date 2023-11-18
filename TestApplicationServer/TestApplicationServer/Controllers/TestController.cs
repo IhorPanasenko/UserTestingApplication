52ff61849630d32f3d5d6cf199986ab21e76c31a
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 20:41:20 +0200

    CreateTestController

---
+++ TestApplicationServer/TestApplicationServer/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApplicationServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task<IActionResult> GetById(int testId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
