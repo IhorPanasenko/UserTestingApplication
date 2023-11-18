cf304ef93d233651e5b218e8ccfb8acd0f335133
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 00:26:03 +0200

    Add GetByTest Function
    For I question repository

--- TestApplicationServer/DAL/Interfaces/IQuestionRepository.cs
+++ TestApplicationServer/DAL/Interfaces/IQuestionRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace DAL.Interfaces
+﻿using Core.Models;
+
+namespace DAL.Interfaces
 {
     public interface IQuestionRepository
     {
         public Task<int> CountTestQuestions(int testId);
+        public Task<List<Question>?> GetbyTest(int testId);
     }
 }
