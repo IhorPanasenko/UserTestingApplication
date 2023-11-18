ea5323c541c65b867786fb71017c0a69ad5cd58a
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 11:50:42 +0200

    Add fields to question model

--- TestApplicationServer/Core/Models/Question.cs
+++ TestApplicationServer/Core/Models/Question.cs
@@ -1,13 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core.Models
 {
     public class Question
     {
+        [Key]
+        public int QuestionId { get; set; }
+
+        [ForeignKey("Test")]
+        public int TestId { get; set; }
+
+        [Required]
+        public string QuestionText { get; set; } = string.Empty;
+
+        [ForeignKey("QuestionType")]
+        public int QuestionTypeId { get; set; }
+
+        public int Points { get; set; }
 
     }
 }
