bef648c1ba73931a7943940f2283ed1bbbda1ae9
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 11:53:20 +0200

    Create question option model

---
+++ TestApplicationServer/Core/Models/QuestionOption.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class QuestionOption
+    {
+        [Key]
+        public int OptionId { get; set; }
+
+        [ForeignKey("Question")]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public string OptionText { get; set; } = string.Empty;
+
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ TestApplicationServer/Core/Models/UserAnswer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class UserAnswer
+    {
+        [Key]
+        public int UserAnswerId { get; set; }
+
+        [ForeignKey("UserTest")]
+        public int UserTestId { get; set; }
+
+        [ForeignKey("Question")]
+        public int QuestionId { get; set; }
+
+        [ForeignKey("QuestionOption")]
+        public int? UserAnswerOptionId { get; set; }
+
+        public string UserAnswerText { get; set; } = string.Empty;
+    }
+}
