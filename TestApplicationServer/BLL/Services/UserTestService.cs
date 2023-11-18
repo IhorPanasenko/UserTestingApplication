b18ec89debbbe440e7a82ec4d1c8b94eefcd97c2
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 19:12:27 +0200

    Implement IUserTestService

--- TestApplicationServer/BLL/Services/UserTestService.cs
+++ TestApplicationServer/BLL/Services/UserTestService.cs
@@ -13,17 +13,35 @@ namespace BLL.Services
     public class UserTestService: IUserTestService
     {
         public readonly IUserTestRepository userTestRepository;
+        public readonly IUserRepository userRepository;
         public readonly ILogger<UserTestService> logger;
 
-        public UserTestService(ILogger<UserTestService> logger, IUserTestRepository userTestRepository)
+        public UserTestService(ILogger<UserTestService> logger, IUserTestRepository userTestRepository, IUserRepository userRepository)
         {
             this.logger = logger;
             this.userTestRepository = userTestRepository;
+            this.userRepository = userRepository;
         }
 
-        public Task<List<UserTest>> GetUserTests(string userId)
+        public async Task<List<UserTest>?> GetUserTests(string userId)
         {
-           throw new NotImplementedException();
+            try
+            {
+                var user =  await userRepository.GetById(userId);
+
+                if (user is null)
+                {
+                    throw new ArgumentException($"No user with Id {userId}");
+                }
+
+                var userTests = await userTestRepository.GetUserTests(userId);
+                return userTests;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Data access layer error:\n{ex.Message}");
+                return null;
+            }
         }
     }
 }
