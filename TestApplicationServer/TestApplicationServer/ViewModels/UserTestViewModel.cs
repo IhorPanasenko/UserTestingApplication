c25d283f30c13cb77a06f664cc2599747251316d
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 19:12:31 +0200

    Create UserTestViewModel.cs

---
+++ TestApplicationServer/TestApplicationServer/ViewModels/UserTestViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TestApplicationServer.ViewModels
+{
+    public class UserTestViewModel
+    {
+        public int UserTestId { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        public int? Mark { get; set; }
+
+        public int TestId { get; set; }
+
+        public string TestTitle { get; set; } = string.Empty;
+
+        public int NumberOfQuestions { get; set; }
+
+    }
+}
