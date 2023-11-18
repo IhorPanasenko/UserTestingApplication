d613d0b38836b1b76bcc87d3e829597df1d59a96
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-18 20:06:59 +0200

    Implement IQuestionService

---
+++ TestApplicationServer/BLL/Services/QuestionService.cs
@@ -0,0 +1,47 @@
+﻿using BLL.Interfaces;
+using DAL.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private readonly ILogger<QuestionService> logger;
+        private readonly IQuestionRepository questionRepository;
+        private readonly ITestRepository testRepository;
+
+        public QuestionService(IQuestionRepository questionRepository, ILogger<QuestionService> logger, ITestRepository testRepository)
+        {
+            this.questionRepository = questionRepository;
+            this.logger = logger;
+            this.testRepository = testRepository;
+        }
+
+        public async Task<int?> CountTestQuestions(int testId)
+        {
+            try
+            {
+                var test = testRepository.GetById(testId);
+
+                if(test is null)
+                {
+                    throw new ArgumentException($"No test with Id: {testId}");
+                }
+
+                var numberOfQuestions = await questionRepository.CountTestQuestions(testId);
+                return numberOfQuestions;
+            }
+            catch(Exception ex)
+            {
+                logger.LogError($"Error in data access layer: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
