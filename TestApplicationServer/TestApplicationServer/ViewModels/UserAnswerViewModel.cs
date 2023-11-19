4b69a1bb02f2aff7e251e79fc56f8c842d84ec15
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 09:45:31 +0200

    Create UserAnswerViewModel

---
+++ TestApplicationServer/TestApplicationServer/ViewModels/UserAnswerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Core.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApplicationServer.ViewModels
+{
+    public class UserAnswerViewModel
+    {
+        public int UserAnswerId { get; set; }
+
+        public string UserAnswerText { get; set; } = string.Empty;
+
+        public int UserTestId { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public int? UserAnswerOptionId { get; set; }
+
+        public QuestionOptionViewModel? QuestionOption { get; set; }
+    }
+}
