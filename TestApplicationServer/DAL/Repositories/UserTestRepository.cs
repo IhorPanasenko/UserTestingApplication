7d3ddd460bbb3f6ff9529c8e855b3a9893cd57a7
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 11:48:27 +0200

    Implement CreateUserTest function

--- TestApplicationServer/DAL/Repositories/UserTestRepository.cs
+++ TestApplicationServer/DAL/Repositories/UserTestRepository.cs
@@ -3,17 +3,16 @@ using DAL.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
-
 namespace DAL.Repositories
 {
     public class UserTestRepository : IUserTestRepository
     {
-        private readonly ApplicationDbContext applicationDbContext;
+        private readonly ApplicationDbContext dbContext;
 
         private readonly ILogger<UserTestRepository> logger;
         public UserTestRepository(ApplicationDbContext applicationDbContext, ILogger<UserTestRepository> logger)
         {
-            this.applicationDbContext = applicationDbContext;
+            this.dbContext = applicationDbContext;
             this.logger = logger;
         }
 
@@ -21,13 +20,28 @@ namespace DAL.Repositories
         {
             try
             {
-                var userTests =  await applicationDbContext.UserTests.Where(ut=> ut.AppUserId == userId).ToListAsync();
+                var userTests =  await dbContext.UserTests.Where(ut=> ut.AppUserId == userId).ToListAsync();
                 return userTests;
             }
             catch (Exception ex)
             {
                 logger.LogError(ex.Message);
                 return null;
+            }
+        }
+
+        public async Task<bool> CreateUserTest(UserTest userTest)
+        {
+            try
+            {
+                dbContext.UserTests.Add(userTest);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception e)
+            {
+                logger.LogError($"Error happend while trying to add new UserTest: {e.Message}");
+                return false;
             }
         }
     }
