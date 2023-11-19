4702b558e9c6b3e6c62ba57963c259b7dd9ec190
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 11:41:44 +0200

    Rename model

---
+++ TestApplicationServer/TestApplicationServer/ViewModels/PassingTest/CreatingUserAnswerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestApplicationServer.ViewModels.PassingTest
+{
+    public class CreatingUserAnswerViewModel
+    {
+        public string UserAnswerText { get; set; } = string.Empty;
+
+        [Required]
+        public int UserTestId { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        public int? UserAnswerOptionId { get; set; }
+    }
+}
