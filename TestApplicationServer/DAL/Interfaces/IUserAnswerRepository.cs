932b998590997d7877420bf75430c1cb199e8cae
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 11:41:56 +0200

    Create IUserAnswerRepository.cs

---
+++ TestApplicationServer/DAL/Interfaces/IUserAnswerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Models;
+
+namespace DAL.Interfaces
+{
+    public interface IUserAnswerRepository
+    {
+        public Task<bool> CreateUserAnswer(UserAnswer userAnswer);
+
+        public Task<bool> CreateManyUserAnswer(List<UserAnswer> userAnswers);   
+    }
+}
