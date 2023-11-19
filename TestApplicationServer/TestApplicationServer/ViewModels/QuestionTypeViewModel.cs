8ddf1f29152660f363fe69cb071a5fc448641f34
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 09:05:35 +0200

    Create QuestionTypeViewModel.cs

---
+++ TestApplicationServer/TestApplicationServer/ViewModels/QuestionTypeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Core.Enums;
+
+namespace TestApplicationServer.ViewModels
+{
+    public class QuestionTypeViewModel
+    {
+        public int QuestionTypeId { get; set; }
+
+        public EnumQuestionType QuestionTypeName { get; set; }
+    }
+}
