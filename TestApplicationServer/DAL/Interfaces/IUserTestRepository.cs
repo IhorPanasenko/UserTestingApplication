0ccadd321ad650dd103f185d16ebf83695ca0828
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 11:48:49 +0200

    Add CreateUserTest for IUserTestService

--- TestApplicationServer/DAL/Interfaces/IUserTestRepository.cs
+++ TestApplicationServer/DAL/Interfaces/IUserTestRepository.cs
@@ -5,5 +5,7 @@ namespace DAL.Interfaces
     public interface IUserTestRepository
     {
         public Task<List<UserTest>?> GetUserTests(string userId);
+
+        public Task<bool> CreateUserTest(UserTest userTest);
     }
 }
