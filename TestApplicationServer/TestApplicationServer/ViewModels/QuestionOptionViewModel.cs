66b55239f4b3ee3903b63d7231015ee791adffa9
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 09:05:39 +0200

    Create QuestionOptionViewModel.cs

---
+++ TestApplicationServer/TestApplicationServer/ViewModels/QuestionOptionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestApplicationServer.ViewModels
+{
+    public class QuestionOptionViewModel
+    {
+        public int OptionId { get; set; }
+
+        public string OptionText { get; set; } = string.Empty;
+
+        public bool IsCorrect { get; set; }
+
+        public int QuestionId { get; set; }
+    }
+}
