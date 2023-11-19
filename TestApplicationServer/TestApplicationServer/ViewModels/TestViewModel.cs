856b7c78da969d1dde3ce0238614c35c35b1f269
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 09:35:20 +0200

    Update TestViewModel.cs

--- TestApplicationServer/TestApplicationServer/ViewModels/TestViewModel.cs
+++ TestApplicationServer/TestApplicationServer/ViewModels/TestViewModel.cs
@@ -5,6 +5,6 @@
         public int TestId { get; set; }
         public string TestName { get; set; } = string.Empty;
         public List<QuestionViewModel> Questions { get; set; } = new List<QuestionViewModel>();
-        public List<UserTestViewModel> UserTests { get; set; } = new List<UserTestViewModel>();
+        public UserTestViewModel? UserTest { get; set; }
     }
 }
