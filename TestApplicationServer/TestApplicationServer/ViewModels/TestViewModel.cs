8b85d5b2becb7f87d6a654a77b89e0024ae91ec0
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 09:05:29 +0200

    Create TestViewModel.cs

---
+++ TestApplicationServer/TestApplicationServer/ViewModels/TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestApplicationServer.ViewModels
+{
+    public class TestViewModel
+    {
+        public int TestId { get; set; }
+        public string TestName { get; set; } = string.Empty;
+        public List<QuestionViewModel> Questions { get; set; } = new List<QuestionViewModel>();
+        public List<UserTestViewModel> UserTests { get; set; } = new List<UserTestViewModel>();
+    }
+}
