12c535b5770e6ef174773029a70bcdf9cb8c8cd1
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 19:58:38 +0200

    Add function to IUserTestService

--- TestApplicationServer/DAL/Interfaces/IUserAnswerRepository.cs
+++ TestApplicationServer/DAL/Interfaces/IUserAnswerRepository.cs
@@ -4,6 +4,8 @@ namespace DAL.Interfaces
 {
     public interface IUserAnswerRepository
     {
+        public Task<List<UserAnswer>?> GetForTest(int userTestId);
+
         public Task<bool> CreateUserAnswer(UserAnswer userAnswer);
 
         public Task<bool> CreateManyUserAnswer(List<UserAnswer> userAnswers);   
