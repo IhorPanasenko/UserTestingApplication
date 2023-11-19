3a9957c5545fc7b7481e10ca30e48b36e920824b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-19 10:07:07 +0200

    Update UserTestService.cs

--- TestApplicationServer/BLL/Services/UserTestService.cs
+++ TestApplicationServer/BLL/Services/UserTestService.cs
@@ -9,13 +9,15 @@ namespace BLL.Services
     {
         private readonly IUserTestRepository userTestRepository;
         private readonly IUserRepository userRepository;
+        private readonly ITestRepository testRepository;
         private readonly ILogger<UserTestService> logger;
 
-        public UserTestService(ILogger<UserTestService> logger, IUserTestRepository userTestRepository, IUserRepository userRepository)
+        public UserTestService(ILogger<UserTestService> logger, IUserTestRepository userTestRepository, IUserRepository userRepository, ITestRepository testRepository)
         {
             this.logger = logger;
             this.userTestRepository = userTestRepository;
             this.userRepository = userRepository;
+            this.testRepository = testRepository;
         }
 
         public async Task<List<UserTest>?> GetUserTests(string userId)
@@ -30,6 +32,18 @@ namespace BLL.Services
                 }
 
                 var userTests = await userTestRepository.GetUserTests(userId);
+
+                if(userTests is null)
+                {
+                    logger.LogError($"Test for user {userId} returned null");
+                    return userTests;
+                }
+
+                foreach(var ut in userTests)
+                {
+                    ut.Test = await testRepository.GetById(ut.TestId);
+                }
+
                 return userTests;
             }
             catch (Exception ex)
