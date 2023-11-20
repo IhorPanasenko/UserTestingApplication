845654fc6dbeedbb928935653a47f475e77e4446
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-20 11:29:03 +0200

    Create Login.jsx

---
+++ client-test-app/src/pages/login/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import LoginForm from "../../components/forms/loinForm/LoginForm";
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <div className="d-flex flex-column justify-content-between flex-grow-1">
+        <LoginForm />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Login;
