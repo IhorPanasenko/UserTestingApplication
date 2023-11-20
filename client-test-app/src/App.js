1154ca7bf084abb9e8c26ee4a51a9a556f9b17f3
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-20 11:20:34 +0200

    Add routing features

--- client-test-app/src/App.js
+++ client-test-app/src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <div className="d-flex flex-column min-vh-100">
+    <Router>
+      <Routes>
+       
+      </Routes>
+    </Router>
+  </div>
   );
 }
 
