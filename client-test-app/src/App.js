dc4ed7ccdddc6f0ab5a80613fe121d8c58d1871b
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-20 11:48:41 +0200

    Add new routes

--- client-test-app/src/App.js
+++ client-test-app/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Login from "./pages/login/Login";
+import UserTests from "./pages/userTests/UserTests";
+import About from "./pages/about/About";
 
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
-    <Router>
-      <Routes>
-      <Route path="/" Component={Login} />
-      </Routes>
-    </Router>
-  </div>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Login} />
+          <Route path="/UserTests" Component={UserTests} />
+          <Route path="/About/" Component={About} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
