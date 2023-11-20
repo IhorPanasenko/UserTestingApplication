60132e699435de274929442f231318963292d96e
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-20 11:23:17 +0200

    Create footer

---
+++ client-test-app/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer bg-dark" >
+            <p>&copy; User Test Application  2023</p>
+        </footer>
+    )
+}
+
+export default Footer
