f99e8a704fb123f188e1c58d52ba0ca88d04c600
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-20 11:29:57 +0200

    Add loginpage to routes

--- client-test-app/src/App.js
+++ client-test-app/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import './App.css';
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
     <Router>
       <Routes>
-       
+      <Route path="/" Component={Login} />
       </Routes>
     </Router>
   </div>
