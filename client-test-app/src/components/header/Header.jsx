e1d63cd3909c0fb212f92dc690578ba59f5e8f43
Ihor Panasenko <ihor.panasenko1@nure.ua>
2023-11-20 11:25:22 +0200

    Create header

---
+++ client-test-app/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+    return (
+        <Navbar className='fs-2' bg="dark" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand className='fs-2 m-auto' href="#home">TestsApp</Navbar.Brand>
+                <Nav className="m-auto">
+                    <Nav.Link className='m-2 p-4 pb-0 pt-0' href="/">Login</Nav.Link>
+                    <Nav.Link className='m-2 p-4 pb-0 pt-0' href="#">My tests</Nav.Link>
+                    <Nav.Link className='m-2 p-4 pb-0 pt-0' href="#">About</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
